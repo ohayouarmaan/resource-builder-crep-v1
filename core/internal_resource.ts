6e4b3b20f05eef6ea58736375d7f963c1e7bd6e4
Armaan Gupta <ohayouarmaan@gmail.com>
2024-06-17 13:49:00 +0530

    comment: basic dependency resolving abstract method

--- core/internal_resource.ts
+++ core/internal_resource.ts
@@ -1,4 +1,6 @@
 export default abstract class InternalResource<C> {
     abstract run(): void;
     abstract register_config(config: C): void;
+    // The below method should be able to get all the dependencies from the resource (if any) and then install it in the main resource.
+    // abstract resolve_dependencies(): void;
 }
--- core/resource.ts
+++ core/resource.ts
@@ -10,9 +10,6 @@ class Resource {
     this.config = config;
   }
   async run(logic: Record<string, Logic>) {
-    //TODO: Add a simple switch case which will check the resource type and create and run the resource accordingly, also make sure
-    //to make it memory effecient do not make it so that a lot of useless objects are created which are not needed at all.
-    //we can lazily import the functions, based on the resourceType to make sure that we are not creating memory overhead if not needed.
     switch (this.type) {
       case "express":
         await this.createServerResource(logic);
