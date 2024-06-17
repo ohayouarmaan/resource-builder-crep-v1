3fb1d971709af3c4441865f93e0791c99af39854
Armaan Gupta <ohayouarmaan@gmail.com>
2024-06-17 15:21:53 +0530

    feat: base mongodb dependency added, still need to test it and do the other things

--- core/dependency.ts
+++ core/dependency.ts
@@ -6,12 +6,16 @@ interface IDependency<C> {
     config: C
 }
 
-export default class Dependency<C> {
+//TODO: Add an ID field which can be reffered by logic to use this dependency which should be resolved in the Logic class.
+//we can loop through the dependencies required in the logic and add an object which we can bind to the logic function so that the
+//user can use the required dependencies as an object.
+export default class Dependency<C, T> {
     public type: string;
     public config: C;
     public comment: string | undefined;
     //@ts-ignore ignore this <UNUSED ERRROR> saving this as a property because it may be required in the future.
     private parent: InternalResource<unknown>;
+    public core: T | undefined;
 
     constructor(dependency: IDependency<C>) {
         this.type = dependency.type;
@@ -19,7 +23,12 @@ export default class Dependency<C> {
         this.comment = dependency.comment;
     }
 
+    // Registers the parent resource, just in case we might need to use it somehow.
     register_parent<IC>(parent: InternalResource<IC>) {
         this.parent = parent;
+    }
+
+    async create_core(cb: (x: Dependency<C, T>) => Promise<T>) {
+        this.core = await cb(this);
     }
 }
