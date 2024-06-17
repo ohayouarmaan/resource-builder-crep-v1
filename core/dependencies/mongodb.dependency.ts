f484ae01309ac425026b4c5561568559c7bdf4ae
Armaan Gupta <ohayouarmaan@gmail.com>
2024-06-17 17:32:20 +0530

    fix: added prettified code

--- core/dependencies/mongodb.dependency.ts
+++ core/dependencies/mongodb.dependency.ts
@@ -2,42 +2,47 @@ import mongoose from "mongoose";
 import Dependency from "../dependency";
 
 interface IMongoDBConfig {
-    host: string;
-    port: string;
-    username: string;
-    password: string;
-    db_name: string;
+  host: string;
+  port: string;
+  username: string;
+  password: string;
+  db_name: string;
 }
 
-export default class MongodbDependency extends Dependency<IMongoDBConfig, mongoose.Connection> {
-    constructor(config: IMongoDBConfig, id: string, comment?: string) {
-        super({
-            type: "mongodb",
-            comment: comment,
-            config,
-            id,
-        });
-    }
+export default class MongodbDependency extends Dependency<
+  IMongoDBConfig,
+  mongoose.Connection
+> {
+  constructor(config: IMongoDBConfig, id: string, comment?: string) {
+    super({
+      type: "mongodb",
+      comment: comment,
+      config,
+      id,
+    });
+  }
 
-    async connect() {
-        this.create_core(async (_x): Promise<mongoose.Connection> => {
-            try{
-                const db  = await mongoose.connect(`mongodb://${this.config.username}:${this.config.password}@${this.config.host}:${this.config.port}/${this.config.db_name}`);
-                return db.connection;
-            } catch(e) {
-                console.error(e);
-                throw new Error((e as Error).message);
-            }
-        });
-    }
+  async connect() {
+    this.create_core(async (_x): Promise<mongoose.Connection> => {
+      try {
+        const db = await mongoose.connect(
+          `mongodb://${this.config.username}:${this.config.password}@${this.config.host}:${this.config.port}/${this.config.db_name}`,
+        );
+        return db.connection;
+      } catch (e) {
+        console.error(e);
+        throw new Error((e as Error).message);
+      }
+    });
+  }
 
-    async get_collection(collection_name: string) {
-        if(this.core) {
-            return this.core.collections[collection_name];
-        } else {
-            await this.connect();
-            // using the not null constraint here because we are connecting above and the core should be filled with the required details.
-            return this.core!.collections[collection_name];
-        }
+  async get_collection(collection_name: string) {
+    if (this.core) {
+      return this.core.collections[collection_name];
+    } else {
+      await this.connect();
+      // using the not null constraint here because we are connecting above and the core should be filled with the required details.
+      return this.core!.collections[collection_name];
     }
+  }
 }
--- core/dependency.ts
+++ core/dependency.ts
@@ -1,34 +1,37 @@
 import InternalResource from "./internal_resource";
 
 interface IDependency<C> {
-    type: string;
-    comment?: string;
-    config: C
+  type: string;
+  comment?: string;
+  id: string;
+  config: C;
 }
 
 //TODO: Add an ID field which can be reffered by logic to use this dependency which should be resolved in the Logic class.
 //we can loop through the dependencies required in the logic and add an object which we can bind to the logic function so that the
 //user can use the required dependencies as an object.
 export default class Dependency<C, T> {
-    public type: string;
-    public config: C;
-    public comment: string | undefined;
-    //@ts-ignore ignore this <UNUSED ERRROR> saving this as a property because it may be required in the future.
-    private parent: InternalResource<unknown>;
-    public core: T | undefined;
+  public type: string;
+  public config: C;
+  public comment: string | undefined;
+  //@ts-ignore ignore this <UNUSED ERRROR> saving this as a property because it may be required in the future.
+  private parent: InternalResource<unknown>;
+  public core: T | undefined;
+  readonly id: string;
 
-    constructor(dependency: IDependency<C>) {
-        this.type = dependency.type;
-        this.config = dependency.config
-        this.comment = dependency.comment;
-    }
+  constructor(dependency: IDependency<C>) {
+    this.type = dependency.type;
+    this.config = dependency.config;
+    this.id = dependency.id;
+    this.comment = dependency.comment;
+  }
 
-    // Registers the parent resource, just in case we might need to use it somehow.
-    register_parent<IC>(parent: InternalResource<IC>) {
-        this.parent = parent;
-    }
+  // Registers the parent resource, just in case we might need to use it somehow.
+  register_parent<IC>(parent: InternalResource<IC>) {
+    this.parent = parent;
+  }
 
-    async create_core(cb: (x: Dependency<C, T>) => Promise<T>) {
-        this.core = await cb(this);
-    }
+  async create_core(cb: (x: Dependency<C, T>) => Promise<T>) {
+    this.core = await cb(this);
+  }
 }
